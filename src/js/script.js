abdedb41a3871a8ad215cecfcc5ed43c91f7a9e3
Aleksandr <ganja.sasha@gmail.com>
2022-06-24 01:25:36 +0300

    carousel

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }
+        ]
+    });
+  });
