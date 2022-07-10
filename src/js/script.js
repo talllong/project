162eba786e5841c869ba7cfdef6698b3e3f9ef1f
Aleksandr <ganja.sasha@gmail.com>
2022-07-11 00:01:25 +0300

    add icons

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel_inner').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="src/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="src/icons/right.png"></button>',
         responsive: [
         {
             breakpoint: 992,
